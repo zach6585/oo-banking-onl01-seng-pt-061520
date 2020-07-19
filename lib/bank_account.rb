835dbfdcb7a3017aba84af4cd66cb69a4de665a4
zachary mines <zmines@wesleyan.edu>
2020-07-19 02:54:21 +0000

    Automatically backed up by Learn

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -1,3 +1,17 @@
 class BankAccount
-
+  attr_accessor :balance, :status
+  attr_reader :name
+  def initialize(name)
+    @name = name
+    @balance = 1000
+    @status = "open"
+  end 
+  
+  def deposit(money)
+    @balance += money 
+  end 
+  
+  def display_balance
+    "Your balance is $#{self.balance}"
+  end 
 end
