28c6775103a656c4d9e85d8fff5d335593e2eb92
zachary mines <zmines@wesleyan.edu>
2020-07-19 02:55:52 +0000

    Automatically backed up by Learn

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -16,7 +16,7 @@ class BankAccount
   end 
   
   def valid?
-    if @status == "open" && @balance >= 0 
+    if @status == "open" && @balance > 0 
       return true 
     else 
       return false 
