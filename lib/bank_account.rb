c577419744ab1ad98fcfdcf6a5fad18834f4b63f
zachary mines <zmines@wesleyan.edu>
2020-07-19 02:55:16 +0000

    Automatically backed up by Learn

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -14,4 +14,10 @@ class BankAccount
   def display_balance
     "Your balance is $#{self.balance}."
   end 
+  
+  def valid?
+    if @status == "open" && @balance >= 0 
+      return True 
+    else 
+      return False 
 end
