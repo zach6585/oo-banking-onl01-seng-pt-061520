5f12471d9cfe45568afc1392482c0bb56fd95412
zachary mines <zmines@wesleyan.edu>
2020-07-19 02:59:23 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -1,9 +1,9 @@
 class Transfer
   attr_accessor :sender, :receiver, :transfer_amount, :status
-  def initialize (sender, receiver, transfer_amount)
+  def initialize (sender, receiver, amount)
     @sender = sender 
     @receiver = receiver
     @status = "pending"
-    @transfer_amount = transfer_amount
+    @amount = amount
   end 
 end
