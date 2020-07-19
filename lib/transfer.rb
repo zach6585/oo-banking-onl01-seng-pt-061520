80941aacd2b4e0e5fa6e1ea2d45ac2de199e2a52
zachary mines <zmines@wesleyan.edu>
2020-07-19 02:57:50 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -1,6 +1,8 @@
 class Transfer
-  def initialize (sender, receiver)
+  def initialize (sender, receiver, @transfer_amount)
+    @sender = sender 
+    @receiver = receiver
     @status = "pending"
-    @transfer_amount = 0 
+    @transfer_amount = transfer_amount
   end 
 end
